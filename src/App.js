8de1752158f3ce033e77f6d8a30495406cb4a0e5
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-09-12 11:59:21 +0200

    starter code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar.jsx'
+import Home from './components/Home.jsx'
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
+			<Home />
 		</div>
 	)
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+const Home = () => {
+	return (
+		<>
+			<h1>Home</h1>
+			<br />
+			<section>
+				You should render different components depending on the link that the user clicks
+			</section>
+			<br />
+			<section>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam repellat accusamus
+				consequuntur et deserunt impedit numquam dicta id, vero fugit est iste, inventore beatae
+				ipsum nulla nostrum blanditiis vitae quisquam. Obcaecati animi alias consequuntur possimus
+				vitae. Molestias reiciendis optio corporis quos magnam ex voluptate eum pariatur minus,
+				voluptatum explicabo aliquam quas officia. Reiciendis libero adipisci iste, nesciunt
+				doloremque corrupti id illum neque recusandae molestias aliquid voluptas porro nobis animi!
+				Corporis facilis neque dolorem laboriosam vero aliquam quod qui necessitatibus repellendus
+				nulla architecto sunt voluptas praesentium earum, et ullam ipsum aspernatur minima dolor
+				modi molestiae? Officiis, maxime delectus reiciendis quaerat at, cumque amet laudantium
+				excepturi porro rem saepe sunt voluptate corrupti quisquam architecto officia, esse adipisci
+				odit ipsum corporis placeat cum dignissimos fugit fuga! Quasi incidunt debitis vero nulla et
+				aperiam cum aliquam consectetur molestias quod dignissimos earum beatae blanditiis iure,
+				accusantium delectus, placeat labore similique quos iusto laborum molestiae error! Dolore
+				minima beatae nobis qui neque eligendi, fugiat vero dolorem expedita molestias cum possimus.
+				Fugiat quas, cumque velit minima quidem, debitis consectetur quo veniam ab mollitia rerum
+				deleniti ut exercitationem nobis laborum sequi. Quam unde delectus, qui officia voluptatum
+				neque itaque odio doloribus pariatur perspiciatis dolores similique, praesentium nihil
+				consectetur rerum facilis nam totam autem quas veniam distinctio inventore. Rem voluptas
+				incidunt itaque eligendi? Quis aspernatur dolore veritatis pariatur ratione qui culpa minus
+				quas reiciendis recusandae. Laudantium fuga iusto nostrum officia pariatur obcaecati illo
+				consectetur atque officiis! Fuga, blanditiis totam nobis expedita maiores iusto labore
+				officiis corrupti eos impedit cum!
+			</section>
+		</>
+	)
+}
+export default Home
