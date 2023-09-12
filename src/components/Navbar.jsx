efcec3f27af75e9a5b03ae0ed936c1d96ab5b365
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-09-12 09:58:18 +0200

    starter code4

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+	return (
+		<div className='nav'>
+			<h2>React Routing</h2>
+			<div className='nav-menu'>
+				<span>Projects</span>
+				<span>About</span>
+			</div>
+		</div>
+	)
+}
+export default Navbar
