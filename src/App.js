8cfd235180f21f8876671aa8f8f558b31c5ebbeb
Kiruba Chelladurai <kirubaberlin@gmail.com>
2023-09-12 19:42:54 +0200

    first bit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import './App.css'
-import Navbar from './components/Navbar.jsx'
-import Home from './components/Home.jsx'
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-	return (
-		<div className='App'>
-			<Navbar />
-			<Home />
-		</div>
-	)
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/projects"
+          element={
+            <h1>
+              <Projects />
+            </h1>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
